15857b0553e51e10b407f817d106cc1df73e6ba4
door-yeah <dptjd0216@naver.com>
2026-01-16 00:35:41 +0900

    Feat/#2 백엔드 초기 설정 완료 (Spring Boot 4.0.1, Java 21)

---
+++ backend/my-hobanwoo/src/main/java/com/hobanwoo/MyHobanWooApplication.java
@@ -0,0 +1,13 @@
+package com.hobanwoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHobanWooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHobanWooApplication.class, args);
+	}
+
+}
