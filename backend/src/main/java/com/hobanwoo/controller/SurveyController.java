158a55c5d8e806cbffcf2acc7ef9e44032357ba6
door-yeah <dptjd0216@naver.com>
2026-03-08 01:37:25 +0900

    CORS오류 해결

--- backend/src/main/java/com/hobanwoo/controller/SurveyController.java
+++ backend/src/main/java/com/hobanwoo/controller/SurveyController.java
@@ -7,12 +7,14 @@ import com.hobanwoo.dto.SurveyResultResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.hobanwoo.service.SurveyService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 public class SurveyController {
